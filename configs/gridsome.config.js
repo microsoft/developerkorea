fc621db2f3deb2bb939c96581cb4c61b389f1993
Justin Yoo <justin.yoojh@gmail.com>
2020-12-18 18:03:25 +0900

    Initial Blog Setup (#2)

---
+++ configs/gridsome.config.js
@@ -0,0 +1,117 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  siteDescription: "An open-source framework to generate awesome pages",
+  pathPrefix: '/DeveloperKorea',
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {
+          whitelist: ['svg-inline--fa', 'table', 'table-striped', 'table-bordered', 'table-hover', 'table-sm'],
+          whitelistPatterns: [/fa-$/, /blockquote$/, /code$/, /pre$/, /table$/, /table-$/, /vueperslide$/, /vueperslide-$/]
+        },
+        presetEnvConfig: {},
+        shouldPurge: false,
+        shouldImport: false,
+        shouldTimeTravel: false,
+        shouldPurgeUnusedKeyframes: true,
+      }
+    },
+    {
+      use: 'gridsome-source-static-meta',
+      options: {
+        path: 'content/site/*.json'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Author',
+        path: './content/author/*.md'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Blog',
+        path: './content/blog/**/*.md',
+        refs: {
+          author: 'Author',
+          tags: {
+            typeName: 'Tag',
+            create: true
+          },
+          category: {
+            typeName: 'Category',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'CustomPage',
+        path: './content/pages/*.md'
+      }
+    },
+    {
+      use: 'gridsome-plugin-flexsearch',
+      options: {
+        searchFields: ['title', 'content'],
+        collections: [{
+          typeName: 'Blog',
+          indexName: 'Blog',
+          fields: ['title', 'category', 'excerpt', 'content']
+        }]
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: [
+        'remark-autolink-headings',
+        'remark-attr',
+        ['gridsome-plugin-remark-prismjs-all', {
+          noInlineHighlight: false,
+          showLineNumbers: false,
+        }],
+        require('./packages/gridsome-plugin-remark-figure')
+      ],
+
+      processImages: false
+
+    }
+  },
+  templates: {
+    Blog: [{
+      path: '/posts/:title'
+    }],
+    CustomPage: [{
+      path: '/:title',
+      component: '~/templates/CustomPage.vue'
+    }],
+    Category: [{
+      path: '/category/:title',
+      component: '~/templates/Category.vue'
+    }],
+    Author: [{
+      path: '/author/:name',
+      component: '~/templates/Author.vue'
+    }],
+    Tag: [{
+      path: '/tags/:title',
+      component: '~/templates/Tag.vue'
+    }]
+  },
+  chainWebpack: config => {
+      config.resolve.alias.set('@pageImage', '@/assets/images');
+  }
+}
---
+++ content/author/author1.md
@@ -0,0 +1,10 @@
+---
+id: author1
+name: Troes retardat
+bio: Primis vitae mauris turpis ornare libero odio torquent vehicula proin consequat curabitur mattis
+facebook: https://www.facebook.com
+twitter: https://www.twitter.com
+linkedin: https://www.linkedin.com
+image: ./images/author1.png
+cover: ./cover/benjamin-voros-Lxq_TyMMHtQ-unsplash.jpg
+---
